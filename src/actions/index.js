30a5bf872a5b21cd5f7d151858920fce55139c23
lipho <hello@lipho.net>
2016-09-09 02:33:08 -0700

    redux-prom

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,21 @@
 import secret from '../../secret'
-
-const wtrUrlBase = `api.openweathermap.org/data/2.5/forecast?q=London,us&mode=xml`;
+import axios from 'axios';
 const API_KEY = secret;
 
-export function fetchWeather() {
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+const FETCH_WEATHER = 'FETCH_WEATHER';
+
+/**
+ * Sents a get request to open
+ * @param city
+ * @returns {{type: string}} - action.
+ */
+export function fetchWeather(city) {
+  const url =`${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
  return {
-   type: FETCH_WEATHER
+   type: FETCH_WEATHER,
+   payload: request
  };
 }
