45132094b3604e40e8f02efe9c5f9d476059d4a3
Lipho V <hello@lipho.net>
2016-09-09 13:59:09 -0700

    05-52

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,10 @@
 import secret from '../../secret'
 import axios from 'axios';
+import { FETCH_WEATHER } from '../constants/index'
 const API_KEY = secret;
 
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
-const FETCH_WEATHER = 'FETCH_WEATHER';
 
 /**
  * Sents a get request to open
@@ -14,7 +14,7 @@ const FETCH_WEATHER = 'FETCH_WEATHER';
 export function fetchWeather(city) {
   const url =`${ROOT_URL}&q=${city},us`;
   const request = axios.get(url);
- return {
+  return {
    type: FETCH_WEATHER,
    payload: request
  };
