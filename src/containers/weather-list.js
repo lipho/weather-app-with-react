cecb966e18ec5993a5c3b8236e9d7303a516985a
lipho <hello@lipho.net>
2016-09-10 17:14:33 -0700

    add map, charts

--- src/containers/weather-list.js
+++ src/containers/weather-list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
-import {Sparklines, Sparklinesline} from 'react-sparklines';
+import Chart from '../components/chart';
+import GoogleMap from '../components/google-map';
 
 class WeatherList extends Component {
   /**
@@ -10,34 +11,20 @@ class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
     const temps = cityData.list.map(weather => weather.main.temp);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
+    const { lon, lat } = cityData.city.coord;
+
     return (
       <tr key={name}>
-        <td>{name}</td>
-        <td>
-          <Sparklines height={120} width={180} data={temps}>
-            <Sparklinesline color='red' />
-          </Sparklines>
-        </td>
+        <td><GoogleMap lon={lon} lat={lat}/></td>
+        <td><Chart color="red" data={temps} units="K"/></td>
+        <td><Chart color="green" data={pressures} units="hPa"/></td>
+        <td><Chart color="blue" data={humidities} units="%"/></td>
       </tr>
     );
   }
 
-  executeHeaderFns(headers, config) {
-    var headerContent, processedHeaders = {};
-
-    forEach(headers, function (headerFn, header) {
-      if (isFunction(headerFn)) {
-        headerContent = headerFn(config);
-        if (headerContent != null) {
-          processedHeaders[header] = headerContent;
-        }
-      } else {
-        processedHeaders[header] = headerFn;
-      }
-    });
-
-    return processedHeaders;
-  }
 
   render() {
     return (
@@ -45,10 +32,9 @@ class WeatherList extends Component {
         <thead>
         <tr>
           <th>City</th>
-          <th>Temp</th>
-          <th>Pressure</th>
-          <th>Humidity</th>
-
+          <th>Temp (k)</th>
+          <th>Pressure (hPa)</th>
+          <th>Humidity (%)</th>
         </tr>
         </thead>
         <tbody>
